43992f07cdb8f445822493afd04617b928d416d2
adityachandla <adityachandla@gmail.com>
2020-10-20 16:41:48 +0530

    Initial commit.

---
+++ src/main/java/com/wordsearch/writeservice/WriteserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wordsearch.writeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordsearch/writeservice/WriteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordsearch.writeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
